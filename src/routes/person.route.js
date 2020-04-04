dd94616da6fa07b1da0c4b6c3cedc07453583498
Jhony Rivero <jhony.rivero@hotmail.com>
2020-04-04 15:41:57 -0500

    impletantion of getByFilter, Delete and save

--- src/routes/person.route.js
+++ src/routes/person.route.js
@@ -1,14 +1,18 @@
 import KoaRouter from 'koa-router'
 import PersonController from '../controllers/person.controller'
 import personSchemas from '../schemas/person.schemas'
-import schemaValidator from '../middleware/schemaValidator'
+import schemaValidator from '../utils/schema-validator'
 
 const controller = new PersonController()
 const router = new KoaRouter({ prefix: '/person' })
-const byEmailValidator = schemaValidator(personSchemas.byEmail)
-const postValidator = schemaValidator(personSchemas.post)
+const byEmailValidator = schemaValidator({ params: personSchemas.byEmail })
+const byQueryValidator = schemaValidator({ params: personSchemas.byQuery })
+const postValidator = schemaValidator({ body: personSchemas.post })
+const deleteValidator = schemaValidator({ params: personSchemas.deleteByIndex })
 
-// router.get('person/byEmail', '/:email', byEmailValidator, controller.getByEmail)
 router.get('/:email', byEmailValidator, controller.getByEmail)
+router.get('/:country/:gender/:eye?', byQueryValidator, controller.getByFiler)
 router.post('/', postValidator, controller.save)
+router.delete('delete', '/:index', deleteValidator, controller.delete)
+
 export default router
